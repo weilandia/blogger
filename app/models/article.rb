53755942ab28dfadc0a459312eef69b09402bf79
Nick Weiland <nickweiland@gmail.com>
2016-02-19 01:20:59 -0700

    Add I3

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,14 @@
 class Article < ActiveRecord::Base
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
+  
+  def tag_list
+    self.tags.map(&:name).join(", ")
+  end
+  
+  def tag_list=(tags_string)
+    tag_names = tags_string.split(", ").map(&:downcase).uniq
+    self.tags = tag_names.map { |name| Tag.find_or_create_by(name: name) }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
+  resources :tags
 end
   #this creates:
   #articles GET /articles(.:format) articles#index
